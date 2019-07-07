1e85f09b4f2c7a11010759efc11b47b4bb2ce482
Sangkwun <wer2774@gmail.com>
2019-07-07 14:05:33 +0900

    temp

--- blindness/models.py
+++ blindness/models.py
@@ -1,5 +1,25 @@
-
+from torch import nn
 import torchvision.models as models
+
+
+
+
+class ResNet(nn.Module):
+    def __init__(self, cfg):
+        self.model = models.resnet50(
+            pretrained=False,
+            num_classes=cfg['dataset']['num_class']
+        )
+        cfg
+    
+    def forward(self, input, target):
+        output = self.model(input)
+        loss = None
+        label = None
+        if self.training:
+            return loss
+        return label
+
 
 def build_model(cfg):
     model = models.resnet50(
