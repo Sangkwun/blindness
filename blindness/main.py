4dea1a1852f1420ce3e9b5788e02adad4eec1ac8
Kwon <wer2774@gmail.com>
2019-08-03 17:56:21 +0900

    Exp (#5)
    * add config use_original
    
    * fix json
    
    * add base aug
    
    * fix length for validate
    
    * Base for tta
    
    * add tta
    
    * add upsampling & add remove background
    
    * fix
    
    * add default num_tta
    
    * fix training loss
    
    * override name
    
    * remove multiply loss
    
    * fix loss caculator
    
    * add resize
    
    * fix transforms
    
    * add policy augmentation (#2)
    
    add policy augmentation
    
    * remove file and fix config
    
    * add default config system
    
    * add dataset, class ratio sampler
    
    * change length
    
    * fix resizer
    
    * remove index in validate
    
    * efficientNet (#4)
    
    * efficientNet
    
    * Update configs.py
    
    * Delete base.json
    
    * remove jsons

--- blindness/main.py
+++ blindness/main.py
@@ -6,13 +6,14 @@ import argparse
 import pandas as pd
 import numpy as np
 
+from torch import cuda
 from pathlib import Path
 from shutil import copyfile
 from torch import optim, nn
 from tqdm import tqdm, trange
 from sklearn.metrics import cohen_kappa_score
 
-from .configs import dataset_map
+from .configs import dataset_map, get_cfg
 from .models import build_model
 from .transforms import build_transforms
 from .dataset import build_dataset
@@ -27,7 +28,9 @@ def arg_parser():
     parser = argparse.ArgumentParser()
     arg = parser.add_argument
     arg('mode', choices=['train','valid', 'predict', 'submit'])
-    arg('--config_path', type=str, default='blindness/configs/base.json')
+    arg('--config_path', type=str)
+    arg('--name', type=str)
+    arg('--tta', type=int, default=0)
     arg('--model_path', type=str)
     arg('--predictions', nargs='+')
     arg('--output_path', type=str, default='submission.csv')
@@ -38,23 +41,27 @@ def arg_parser():
 
 def main():
     args = arg_parser()
-    with open(args.config_path, 'r') as f:
-        cfg = json.loads(f.read())
+    
+    cfg = get_cfg(args.config_path)
+    if args.name is not None:
+        cfg['name'] = args.name
 
     if args.mode == 'train':
         train(cfg)
     elif args.mode == 'valid':
         run_valid(cfg, args.model_path)
     elif args.mode == 'predict':
-        predict(cfg, args.model_path)
+        predict(cfg, args.model_path, args.tta)
     elif args.mode == 'submit':
         submit(args.predictions, args.output_path)
     else:
         raise NotImplementedError
 
 def train(cfg):
-    device = torch.device("cuda:0")
-
+    if cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
     train_transform  = build_transforms(cfg, split='train')
     valid_transform  = build_transforms(cfg, split='valid')
 
@@ -111,10 +118,10 @@ def train(cfg):
 
             step += 1
 
-            running_loss += loss.item() * image.size(0)
+            running_loss += loss.item()
 
 
-        epoch_loss = running_loss / len(train_data)
+        epoch_loss = running_loss / len(train_data.dataset)
         loss_list.append(epoch_loss)
         print('Train Loss: {:.4f}'.format(epoch_loss))
     
@@ -129,7 +136,7 @@ def train(cfg):
         # write to tensorboard
         if not ON_KAGGLE:
             if scheduler is not None: lr = scheduler.get_lr()[-1]
-            writer.add_scalar('loss', running_loss, global_step=epoch)
+            writer.add_scalar('loss', epoch_loss, global_step=epoch)
             writer.add_scalar('lr', lr, global_step=epoch)
             writer.add_scalar('valid_loss', valid_loss, global_step=epoch)
             writer.add_scalar('valid_score', valid_score, global_step=epoch)
@@ -152,8 +159,6 @@ def validate(model, valid_data, cfg):
             batch_size = image.size(0)
             outputs, loss = model(image, target, validate=True)
             all_losses.append(loss.detach().cpu().numpy())
-            start_index = i*batch_size
-            end_index = len(valid_data) if (i+1)*batch_size > len(valid_data) else (i+1)*batch_size
 
             all_losses.append(loss)
             all_predictions.append(outputs.cpu().numpy())
@@ -162,7 +167,7 @@ def validate(model, valid_data, cfg):
         all_predictions = np.concatenate(all_predictions)
         all_targets = np.concatenate(all_targets)
 
-        valid_loss = sum([loss.item() for loss in all_losses])
+        valid_loss = sum([loss.item() for loss in all_losses])/len(valid_data.dataset)
         valid_score = cohen_kappa_score(all_targets, np.argmax(all_predictions, axis=1), weights='quadratic')
         print('Validation Loss: {:.4f}'.format(valid_loss))
         print('val_score: {:.4f}'.format(valid_score))
@@ -170,9 +175,14 @@ def validate(model, valid_data, cfg):
     return valid_loss, valid_score
 
 def run_valid(cfg, model_path):
-    device = torch.device("cuda:0")
+    if cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
     valid_transform  = build_transforms(cfg, split='valid')
-    valid_data = build_dataset(cfg, valid_transform, split='valid')
+    valid_data = build_dataset(cfg,
+                               valid_transform,
+                               split='valid')
 
     output_dir = Path('output', cfg['name'])
     output_dir.mkdir(exist_ok=True, parents=True)
@@ -189,12 +199,18 @@ def run_valid(cfg, model_path):
     print('Validation Loss: {:.4f}'.format(valid_loss))
     print('val_score: {:.4f}'.format(valid_score))
 
-def predict(cfg, model_path):
+def predict(cfg, model_path, num_tta):
     cfg['model']['pretrained'] = False
-    device = torch.device("cuda:0")
+    if cuda.is_available():
+        device = torch.device("cuda:0")
+    else:
+        device = torch.device("cpu")
 
     test_transform  = build_transforms(cfg, split='test')
-    test_data = build_dataset(cfg, test_transform, split='test')
+    test_data = build_dataset(cfg,
+                              test_transform,
+                              split='test',
+                              num_tta=num_tta)
     
     model = build_model(cfg, device)
     output_dir = Path('output', cfg['name'])
@@ -235,7 +251,7 @@ def submit(predictions, output):
         df = df.reindex(sample_submission.index)
         dfs.append(df)
     df = pd.concat(dfs)
-    df = df.groupby(level=0).mean()
+    df = df.groupby(level=0).mean() # get mean of predictions
     df = df.apply(get_classes, axis=1)
     df.name = 'diagnosis'
     df.to_csv(output, header=True)
--- blindness/transforms.py
+++ blindness/transforms.py
@@ -3,39 +3,62 @@ import random
 import numpy as np
 
 from PIL import Image
-from torchvision.transforms import Compose, ToTensor, Normalize, RandomResizedCrop, RandomApply
+from torchvision.transforms import Compose, ToTensor, Normalize, RandomResizedCrop, RandomApply, Resize, CenterCrop
 from torchvision.transforms import RandomHorizontalFlip, RandomVerticalFlip, ColorJitter, RandomGrayscale
 
-
-class RandomRotate(object):
-    def __init__(self):
-        self.degrees = [0, 90, 180, 270]
-    def __call__(self, img):
-        degree = random.choice(self.degrees)
-        return img.rotate(degree)
+from .autoaugment import ImageNetPolicy, CIFAR10Policy, SVHNPolicy
 
 
-class BenGrahamAug(object):
+class CropBlack(object):
     def __init__(self, p):
         self.p = p
 
     def __call__(self, img):
         if random.random() < self.p:
+            origin_w, origin_h = img.size
+            cv_img = np.array(img)
+            gray = cv2.cvtColor(cv_img,cv2.COLOR_RGB2GRAY)
+            _,thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
+            contours,hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            cnt = contours[0]
+            x,y,w,h = cv2.boundingRect(cnt)
+            area = (
+                x, y, x+w, y+h
+            )
+            if w > origin_w/2 or h > origin_h/2:
+                img = img.crop(area)
+        return img
+
+
+class RandomRotate(object):
+    def __init__(self, p):
+        self.p = p
+        self.degrees = [0, 90, 180, 270]
+    def __call__(self, img):
+        if random.random() < self.p:
+            degree = random.choice(self.degrees)
+            img =  img.rotate(degree)
+        return img
+
+
+class BenGrahamAug(object):
+    def __init__(self, p):
+        self.p = p
+    def __call__(self, img):
+        if random.random() < self.p:
             w,h = img.size
-            cv_img = np.array(img) 
+            cv_img = np.array(img)
             cv_img = cv2.addWeighted (cv_img, 4, cv2.GaussianBlur(cv_img , (0,0) , w/10) ,-4 ,128)
             img = Image.fromarray(cv_img)
         return img
 
 
-def build_transforms(cfg, split='train'):
-    is_train = split == 'train'
-    input_cfg = cfg['input']
-    width = input_cfg['width']
-    height = input_cfg['height']
-
+def get_transforms(transforms_list,
+                   width,
+                   height,
+                   is_train):
     transforms = []
-    for transform in input_cfg['transforms']:
+    for transform in transforms_list:
         if transform == 'random_resized_crop':
             scale = (0.8, 1.2) if is_train else (1.0, 1.0)
             ratio = (1.0, 1.0) if is_train else (1.0, 1.0)
@@ -45,9 +68,30 @@ def build_transforms(cfg, split='train'):
                     scale=scale,
                     ratio=ratio,
                 )
+                
             )
+        elif transform == 'center_crop' :
+            transforms.append(
+                CenterCrop((700, 700))
+            )
+        elif transform == 'resize':
+            transforms.append(
+                Resize(
+                    (width, height)
+                )
+            )
+        elif transform == 'resize':
+            transforms.append(
+                Resize(
+                    (width, height)
+                )
+            )
+        elif transform == 'crop_black': # crop_black은 첫번째로 넣어줘야함.
+            p = 1.0 if is_train else 1.0
+            transforms.append(CropBlack(p))
         elif transform == 'random_rotate':
-            transforms.append(RandomRotate())
+            p = 0.5 if is_train else 0.25
+            transforms.append(RandomRotate(p))
         elif transform == 'random_vertical_flip':
             p = 0.5 if is_train else 0.25
             transforms.append(RandomVerticalFlip(p))
@@ -67,15 +111,38 @@ def build_transforms(cfg, split='train'):
             p = 0.5 if is_train else 0.25
             transforms.append(RandomGrayscale(p))
         elif transform == 'ben_graham':
-            p = 0.5 if is_train else 0.25
+            p = 1 if is_train else 1
             transforms.append(BenGrahamAug(p))
+        elif transform == 'imagenet_poilcy':
+            transforms.append(ImageNetPolicy())
+        elif transform == 'cifar_policy':
+            transforms.append(CIFAR10Policy())
+        elif transform == 'svhn_policy':
+            transform.append(SVHNPolicy())
         else:
             print(transform)
             raise NotImplementedError
+    return transforms
+
+def build_transforms(cfg, split='train'):
+    is_train = split == 'train'
+    input_cfg = cfg['input']
+    width = input_cfg['width']
+    height = input_cfg['height']
+
+    if split == 'train':
+        transform_list = input_cfg['transforms']
+    else:
+        transform_list = input_cfg['test_transforms']
+
+    transforms = get_transforms(
+        input_cfg['transforms'],
+        width,
+        height,
+        is_train)
 
     transforms += [
         ToTensor(),
         Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]
-
     return Compose(transforms)
